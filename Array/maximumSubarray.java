e539a6fc990fcd724752006cdb1f38bf018a2877
StalinRomi <88613415+StalinRomi@users.noreply.github.com>
2022-06-20 23:30:05 +0530

    max subarray question

--- Array/maximumSubarray.java
+++ Array/maximumSubarray.java
@@ -4,7 +4,21 @@
     
 //     O/P -> 6   (4,-1,2,1  ->   6)
 
+// Add up each element to variable 'sum' and keep maximum of current sum, in variable 'maxNo'  
 
 public int maxSubarray(int[] ar){
+      
+      int len = ar.length;
+      int maxNo = Integer.MIN_VALUE;
+      int sum = 0;
 
+      for (int i = 0; i < len; i++){
+          
+          sum += nums[i];
+          max = Math.max(sum, maxNo);
+        
+        // if sum is a negative no then no point in keeping it
+          if (sum < 0) sum = 0;
+      }
+      return sum;
 }    
