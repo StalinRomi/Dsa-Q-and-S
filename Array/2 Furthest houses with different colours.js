3c0fcee37b451197300140d3768cdbc5cf827fe0
StalinRomi <88613415+StalinRomi@users.noreply.github.com>
2022-08-23 17:22:47 +0530

    new questions added

---
+++ Array/2 Furthest houses with different colours.js
@@ -0,0 +1,6 @@
+// Return the maximum distance between 2 houses with different colors
+
+// Eg.
+// I/O -> colors = [1,1,1,6,1,1,1]
+// O/P -> 3
+// Furthest 2 houses with different colours are house 0 & house 3
---
+++ Array/Max-Product of 2 elements.js
@@ -0,0 +1,5 @@
+//  Return max value of  (nums[i] - 1) * (nums[j] - 1)
+
+// Eg. 1
+// I/O -> nums = [3,4,5,2]
+// O/P -> 12 ( 4-1 * 5-1 = 12 )
