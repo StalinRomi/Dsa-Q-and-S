0a2a55cfb037d8f7c35095dc8b4d50008b2d909c
StalinRomi <88613415+StalinRomi@users.noreply.github.com>
2022-08-25 22:53:48 +0530

    New test case added

--- Array/2 Furthest houses with different colours.js
+++ Array/2 Furthest houses with different colours.js
@@ -1,6 +1,11 @@
 // Return the maximum distance between 2 houses with different colors
 
-// Eg.
+// Eg. 1
 // I/O -> colors = [1,1,1,6,1,1,1]
 // O/P -> 3
-// Furthest 2 houses with different colours are house 0 & house 3
+// Furthest 2 houses with different colours are house 0 & house 3.
+
+// Eg. 2
+// I/O -> colors = [0,1]
+// O/P -> 1
+// Furthest 2 houses with different colours are house 0 & house 1. Distance between them is (0 - 1 = 1).
