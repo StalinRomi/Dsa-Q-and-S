77008939ce6a7c064fb932e18da8a86e02bfcea4
StalinRomi <88613415+StalinRomi@users.noreply.github.com>
2022-10-21 21:29:05 +0530

    Brute force solution added

--- Array/Build Array from Permutation.java
+++ Array/Build Array from Permutation.java
@@ -17,3 +17,12 @@
 // ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
 //     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
 //     = [4,5,0,1,2,3]    
+
+public static int[] ans (int[] nums){
+  
+  for (int i = 0; i < ans.length; i++) {
+     
+     ans[i] = nums[nums[i]];
+  }
+  return ans[];
+}
